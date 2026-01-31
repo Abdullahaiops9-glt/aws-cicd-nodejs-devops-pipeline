e434f6124be3a6ecbb856a9dfa917459840eaee6
root <root@ubuntu-VM.local>
2026-01-31 21:58:47 +0500

    Trigger CI/CD deployment v1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain'});
-	res.end('Hello from AWS CI/CD Devops Project🚀\n');
+	res.end('Hello from AWS CI/CD Devops Project🚀 v1\n');
 }).listen(PORT);
 
 console.log(`Server running on port ${PORT}`);
